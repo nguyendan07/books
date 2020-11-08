9a9c5f07cdc51b7c40c40f8f9bfbc04836e9455e
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-09 00:09:12 +0700

    Write unit tests create user.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='dannv',
+            email='dannv@email.com',
+            password='testpass123456'
+        )
+        self.assertEqual(user.username, 'dannv')
+        self.assertEqual(user.email, 'dannv@email.com')
+        self.assertTrue(user.is_active),
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='superadmin',
+            email='admin@superuser.com',
+            password='123456a@'
+        )
+        self.assertEqual(user.username, 'superadmin')
+        self.assertEqual(user.email, 'admin@superuser.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
