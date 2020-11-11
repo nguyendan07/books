71d5e833da70d4691aac306eb7c153176a0b70e5
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-12 00:14:11 +0700

    Testing Templates

--- pages/tests.py
+++ pages/tests.py
@@ -1,1 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomePageTest(SimpleTestCase):
+    def test_homepage_status_code(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_homepage_url_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_homepage_template(self):
+        resp = self.client.get('/')
+        self.assertTemplateUsed(resp, 'home.html')
+        self.assertContains(resp, 'Homepage')
+        self.assertNotContains(resp, 'Hi there! I should not be on the page.')        
