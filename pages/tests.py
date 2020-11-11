2e368a45795e8ac185ca7b83adfff15e1bf185d7
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-12 00:25:26 +0700

    Check HomePageView “resolves” a given URL path

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,7 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 
 # class HomePageTest(SimpleTestCase):
@@ -32,3 +34,8 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+    
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
