f57aa854867d7bc501d4e40df1b3bd793b6f4b95
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-12 00:19:53 +0700

    Using setUp method

--- pages/tests.py
+++ pages/tests.py
@@ -2,17 +2,33 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 
-class HomePageTest(SimpleTestCase):
+# class HomePageTest(SimpleTestCase):
+#     def test_homepage_status_code(self):
+#         resp = self.client.get('/')
+#         self.assertEqual(resp.status_code, 200)
+    
+#     def test_homepage_url_name(self):
+#         resp = self.client.get(reverse('home'))
+#         self.assertEqual(resp.status_code, 200)
+    
+#     def test_homepage_template(self):
+#         resp = self.client.get('/')
+#         self.assertTemplateUsed(resp, 'home.html')
+#         self.assertContains(resp, 'Homepage')
+#         self.assertNotContains(resp, 'Hi there! I should not be on the page.')
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_homepage_status_code(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
-    
-    def test_homepage_url_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-    
+        self.assertEqual(self.response.status_code, 200)
+
     def test_homepage_template(self):
-        resp = self.client.get('/')
-        self.assertTemplateUsed(resp, 'home.html')
-        self.assertContains(resp, 'Homepage')
-        self.assertNotContains(resp, 'Hi there! I should not be on the page.')        
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
