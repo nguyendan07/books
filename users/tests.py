6664d25ba2c393e9765c290759cce4935cf22b34
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-23 23:38:14 +0700

    Test signup page

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
-class CustomUserTest(TestCase):
+class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
@@ -28,3 +32,24 @@ class CustomUserTest(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.resp = self.client.get(url)
+    
+    def test_signup_template(self):
+        self.assertEqual(self.resp.status_code, 200)
+        self.assertTemplateUsed(self.resp, 'signup.html')
+        self.assertContains(self.resp, 'Sign Up')
+        self.assertNotContains(self.resp, 'Hi there! I should not be on the page.')
+    
+    def test_signup_form(self):
+        form = self.resp.context.get('form')
+        # self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+    
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
