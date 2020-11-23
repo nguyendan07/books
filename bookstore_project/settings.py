4f0039c9364e23d128f621c89b299e9f110f34aa
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-11-24 00:44:42 +0700

    Setting location of static files

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
