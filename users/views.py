da1cce9fb2374f812cc4874d8c168aed3eb4f195
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2021-01-06 23:33:41 +0700

    Update login.html and signup.html with crispy_forms_tags

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from .forms import CustomUserCreationForm
 
 
-class SignupPageView(generic.TemplateView):
+class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
