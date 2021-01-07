e9236204d932fb2110d9092835048d5ebdc399bb
Nguyen Van Dan <dannv6@fpt.com.vn>
2021-01-07 18:02:25 +0700

    update the URL links use django-allauth ’s URL names

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,5 @@ from .views import SignupPageView
 
 
 urlpatterns = [
-    path('signup/', SignupPageView.as_view(), name='signup')
-    
+    # path('signup/', SignupPageView.as_view(), name='signup')
 ]
